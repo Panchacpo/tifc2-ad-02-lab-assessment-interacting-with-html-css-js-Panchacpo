ef40e42d60f54ad915778685fa5bc4be0affd6d4
Francisca Cabrera <francis.cabrera.poblete@gmail.com>
2025-09-03 16:17:08 -0400

    Tarea Terminada

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById("titulo").innerHTML = "Goodbye";
+document.getElementById("titulo2").style.color = "orange";
+ 
+function changeColor() {
+    document.getElementById("titulo5").style.color = "brown";
+}
